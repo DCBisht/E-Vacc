06d7064836caa0ca5c1691a2aa00941cb132ed9e
Divya Chakshu Bisht <65598930+DCBisht@users.noreply.github.com>
2023-12-05 22:02:09 +0530

    Edited database

--- backend-EVacc/index.js
+++ backend-EVacc/index.js
@@ -1,39 +1,10 @@
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-
-const username = encodeURIComponent("chakshubisht459");
-const password = encodeURIComponent("lionking789@#$");
-const uri = `mongodb+srv://${username}:${password}@cluster0.h9p94dc.mongodb.net/?retryWrites=true&w=majority`;
-// let uri =
-//   `mongodb+srv://${username}:${password}@${cluster}/?authSource=${authSource}&authMechanism=${authMechanism}`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
-}
-
-run().catch(console.dir);
-
 const express=require('express');
 
 const app=express();
 const port=3001;
+
+const run=require('../backend-EVacc/DBConnection');
+run().catch(console.dir);
 
 app.use(express.json());
 
